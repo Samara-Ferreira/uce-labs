f607b9bc2a85ad763dc1dfbbcf82452b58cb0aea
Samara <sferreira@ecomp.uefs.br>
2025-09-09 20:34:19 -0300

    ✨ refactor: clean up whitespace in Madhava series implementation

--- lab4/aproximacoes/ex4_madhava.py
+++ lab4/aproximacoes/ex4_madhava.py
@@ -1,4 +1,3 @@
-
 # Lê o número de termos
 n = int(input("Digite o número de termos (n): "))
 
@@ -11,13 +10,13 @@ for i in range(n):
     # Numerador: 1
     # Denominador: (2*i + 1) × 3^i
     denominador_parte1 = 2 * i + 1  # 1, 3, 5, 7, ...
-    denominador_parte2 = 3 ** i     # 3^0, 3^1, 3^2, 3^3, ...
-    
+    denominador_parte2 = 3 ** i  # 3^0, 3^1, 3^2, 3^3, ...
+
     denominador_total = denominador_parte1 * denominador_parte2
-    
+
     # Calcula o termo da série
     termo = 1.0 / denominador_total
-    
+
     # Alterna o sinal: positivo para i par, negativo para i ímpar
     if i % 2 == 0:
         soma_serie += termo
@@ -26,6 +25,7 @@ for i in range(n):
 
 # Calcula π = √12 × soma_serie
 import math
+
 pi_aproximado = math.sqrt(12) * soma_serie
 
 # Exibe o resultado com 8 casas decimais
